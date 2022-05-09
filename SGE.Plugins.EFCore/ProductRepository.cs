e56f53440267af65395270e6fb42838e238059c7
Willian <willian.caldas@hotmail.com>
2022-05-09 12:39:11 -0300

    SGE v.0.006 - Produtos Funcional

--- SGE.Plugins.EFCore/ProductRepository.cs
+++ SGE.Plugins.EFCore/ProductRepository.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SGE.CoreBusiness;
-using SGE.UseCases.PluginInterfaces;
+using SGE.UseCases.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
