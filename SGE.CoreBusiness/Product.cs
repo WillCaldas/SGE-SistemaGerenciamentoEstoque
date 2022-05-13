46f0307f8c15c29c9ab23fd6e067be99a449eafd
Willian <willian.caldas@hotmail.com>
2022-05-13 15:31:35 -0300

    SGE v.0.010 - Validação Preço produto final

--- SGE.CoreBusiness/Product.cs
+++ SGE.CoreBusiness/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SGE.CoreBusiness.Validations;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -18,9 +19,28 @@ namespace SGE.CoreBusiness
         public int Quantity { get; set; }
 
         [Range(0, double.MaxValue, ErrorMessage = "O Preço deve ser maior ou igual a {0}")]
+        [Product_EnsurePriceIsGreaterThanInventoriesPrice]
         public double Price { get; set; }
 
         public List<ProductInventory>? ProductInventories { get; set; }
+
+        public double TotalInventoryCost()
+        {
+            return this.ProductInventories.Sum(x => x.Inventory?.Price * x.InventoryQuantity ?? 0);
+        }
+
+        public bool ValidatePricing()
+        {
+            //não estamos validando o objeto do produto quando os estoques de produtos não estão carregados
+            if (ProductInventories == null || ProductInventories.Count <= 0)
+            {
+                return true;
+            }
+
+            if (this.TotalInventoryCost() > Price) return false;
+
+            return true;
+        }
     }
 }
 
---
+++ SGE.CoreBusiness/Validations/Product_EnsurePriceIsGreaterThanInventoriesPrice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGE.CoreBusiness.Validations
+{
+    internal class Product_EnsurePriceIsGreaterThanInventoriesPrice : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var product = validationContext.ObjectInstance as Product;
+            if (product != null)
+            {
+                if (!product.ValidatePricing())
+                {
+                    return new ValidationResult($"Valor final do produto está abaixo do gasto em itens do Estoque - valor gasto do estoque R${product.TotalInventoryCost()}!",
+                        new[] { validationContext.MemberName});
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
